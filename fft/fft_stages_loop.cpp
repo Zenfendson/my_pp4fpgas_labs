1a1cb7bf2a0d03992023a7d80f9e7000f9ca37f0
Zenfendson <znxntt12@163.com>
2021-03-29 23:25:43 +0800

    Update fft

--- fft/fft_stages_loop.cpp
+++ fft/fft_stages_loop.cpp
@@ -11,11 +11,11 @@ unsigned int reverse_bits(unsigned int input) {
 	return rev;
 }
 
-void bit_reverse(float X_R[SIZE], float X_I[SIZE],
-		 float OUT_R[SIZE], float OUT_I[SIZE]) {
+void bit_reverse(DTYPE X_R[SIZE], DTYPE X_I[SIZE],
+		 DTYPE OUT_R[SIZE], DTYPE OUT_I[SIZE]) {
   unsigned int reversed;
   unsigned int i;
-  float temp;
+  DTYPE temp;
   
   for (int i = 0; i < SIZE; i++) {
 	  reversed = reverse_bits(i); // Find the bit reversed index
@@ -33,8 +33,8 @@ void bit_reverse(float X_R[SIZE], float X_I[SIZE],
 	}
 }
 
-void fft_stage(int stage, float X_R[SIZE], float X_I[SIZE],
-		     float Out_R[SIZE], float Out_I[SIZE]) {
+void fft_stage(int stage, DTYPE X_R[SIZE], DTYPE X_I[SIZE],
+		     DTYPE Out_R[SIZE], DTYPE Out_I[SIZE]) {
   int DFTpts = 1 << stage;    // DFT = 2^stage = points in sub DFT
   int numBF = DFTpts / 2;     // Butterfly WIDTHS in sub-DFT
   int step = SIZE >> stage;
@@ -44,15 +44,15 @@ void fft_stage(int stage, float X_R[SIZE], float X_I[SIZE],
     // Compute butterflies that use same W**k
   dft_loop:
       for(int t = 0; t < step; t++) {
+#pragma HLS PIPELINE II=128
           int i = j + t*DFTpts;
           //    for (int i = j; i < SIZE; i += DFTpts) {
-#pragma HLS pipeline
           int k = j*step;
-      float c = W_real[k];
-      float s = W_imag[k];
+      DTYPE c = W_real[k];
+      DTYPE s = W_imag[k];
       int i_lower = i + numBF; // index of lower point in butterfly
-      float temp_R = X_R[i_lower] * c - X_I[i_lower] * s;
-      float temp_I = X_I[i_lower] * c + X_R[i_lower] * s;
+      DTYPE temp_R = X_R[i_lower] * c - X_I[i_lower] * s;
+      DTYPE temp_I = X_I[i_lower] * c + X_R[i_lower] * s;
       Out_R[i_lower] = X_R[i] - temp_R;
       Out_I[i_lower] = X_I[i] - temp_I;
       Out_R[i] = X_R[i] + temp_R;
@@ -61,17 +61,14 @@ void fft_stage(int stage, float X_R[SIZE], float X_I[SIZE],
   }
 }
 
-void fft_streaming(float X_R[SIZE], float X_I[SIZE], float OUT_R[SIZE], float OUT_I[SIZE])
+void fft_streaming(DTYPE X_R[SIZE], DTYPE X_I[SIZE], DTYPE OUT_R[SIZE], DTYPE OUT_I[SIZE])
 {
-  #pragma HLS dataflow
-  float Stage_R[M][SIZE], Stage_I[M][SIZE];
-  #pragma HLS array_partition variable=Stage_R dim=1 complete
-  #pragma HLS array_partition variable=Stage_I dim=1 complete
+  DTYPE Stage_R[M][SIZE], Stage_I[M][SIZE];
 
   bit_reverse(X_R, X_I, Stage_R[0], Stage_I[0]);
  stage_loop:
   for (int stage = 1; stage < M; stage++) { // Do M-1 stages of butterflies
-    #pragma HLS unroll
+
     fft_stage(stage, Stage_R[stage-1], Stage_I[stage-1], Stage_R[stage], Stage_I[stage]);
   }
   fft_stage(M, Stage_R[M-1], Stage_I[M-1], OUT_R, OUT_I);
